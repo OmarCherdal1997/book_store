9766eab5f4e1147a9855d3ed3a9935a5bd5bd625
Omar Cherdal <ocherdal@bmat.com>
2023-01-03 10:45:01 +0100

    Prepare template

--- book_shelf/views.py
+++ book_shelf/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+def index (request):
+    all_books = models.Book.objects.all()
+    return render(request,"book_shelf/index.html",{
+        "books": all_books
+    })
